6fdbfc93fb4a93ee8fdb857653c682f8dc06e38d
Bennett Petzold <dansecob.dev@gmail.com>
2024-12-01 11:10:09 -0500

    Day 1 Solution

---
+++ 2024/day1/src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    collections::HashMap,
+    env::args,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use itertools::Itertools;
+
+fn main() {
+    let (mut left, mut right): (Vec<_>, Vec<_>) =
+        BufReader::new(File::open(args().nth(1).unwrap()).unwrap())
+            .lines()
+            .map(|line| {
+                let line = line.unwrap();
+                let mut line = line.split_whitespace();
+                let left = line.next().unwrap();
+                let right = line.next().unwrap();
+                (
+                    str::parse::<usize>(left).unwrap(),
+                    str::parse::<usize>(right).unwrap(),
+                )
+            })
+            .unzip();
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let pairs = left.iter().zip(right.iter());
+    let total_diff: usize = pairs.map(|(l, r)| l.abs_diff(*r)).sum();
+    println!("{total_diff}");
+
+    // Part 2
+    let left_occurs = left.into_iter().chunk_by(|x| *x);
+    let left_occurs = left_occurs.into_iter().map(|(key, group)| ListOccurrence {
+        num: key,
+        repeats: group.count(),
+    });
+    let right_occurs: HashMap<usize, usize> = right
+        .into_iter()
+        .chunk_by(|x| *x)
+        .into_iter()
+        .map(|(key, group)| (key, group.count()))
+        .collect();
+
+    let mut sum = 0;
+    for left_inst in left_occurs {
+        if let Some(right_inst) = right_occurs.get(&left_inst.num) {
+            sum += left_inst.apply(*right_inst);
+        }
+    }
+    println!("{sum}")
+}
+
+#[derive(Debug)]
+struct ListOccurrence {
+    pub num: usize,
+    pub repeats: usize,
+}
+
+impl ListOccurrence {
+    pub fn apply(&self, count: usize) -> usize {
+        self.num * self.repeats * count
+    }
+}
