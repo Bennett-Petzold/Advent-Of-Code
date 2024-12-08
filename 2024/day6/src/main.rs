ef50941fc332e94bc1da2236851ad27eb5a0ef65
Bennett Petzold <dansecob.dev@gmail.com>
2024-12-08 16:37:10 -0500

    Solution to Day 6 Part 2
    This one took a long time to solve properly.
    The `print` feature was used for debugging, and generates nicely solved
    paths. Debugged with the help of a second example from Fair-Cause1908.

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    process::exit,
-};
+use std::collections::HashMap;
 
 use advent_rust_lib::{grid::Pos2D, read::input};
 
@@ -80,6 +77,11 @@ pub struct GuardMap {
     max_x: usize,
     max_y: usize,
 }
+
+#[cfg(not(feature = "print"))]
+type SpecLoopReturn = bool;
+#[cfg(feature = "print")]
+type SpecLoopReturn = Option<Vec<Pos2D>>;
 
 impl GuardMap {
     pub fn from_input<S, I>(lines: I) -> Option<Self>
@@ -197,24 +199,139 @@ impl GuardMap {
         unique.len().saturating_sub(1)
     }
 
-    pub fn num_possible_blockages(&self) -> usize {
-        let initial_pos = self.guard.pos();
+    fn speculate_loop(
+        &self,
+        guard: &Guard,
+        next_guard: &Guard,
+        // MUST be sorted
+        unique: &[Guard],
+    ) -> SpecLoopReturn {
+        let block_pos = next_guard.pos();
 
-        let mut guard = self.guard;
-        let mut unique = vec![guard];
-        let mut unique_pos = vec![guard.pos()];
-        let mut num_blocks = 0;
+        // Speculate on current position being rotated by 90
+        let mut speculative_guard = *guard;
+        speculative_guard.rotate();
 
-        let mut obstruction_pos = Vec::new();
+        if let Ok(start_idx) = unique.binary_search_by_key(&speculative_guard.pos(), |x| x.pos()) {
+            for idx in (0..start_idx).rev() {
+                if unique[idx].pos() == speculative_guard.pos() {
+                    if unique[idx] == speculative_guard {
+                        #[cfg(not(feature = "print"))]
+                        return true;
+                        #[cfg(feature = "print")]
+                        return Some(Vec::new());
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            for idx in start_idx..unique.len() {
+                if unique[idx].pos() == speculative_guard.pos() {
+                    if unique[idx] == speculative_guard {
+                        #[cfg(not(feature = "print"))]
+                        return true;
+                        #[cfg(feature = "print")]
+                        return Some(Vec::new());
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        let mut spec_unique = vec![speculative_guard];
 
         #[cfg(feature = "print")]
-        let mut print_unique = vec![initial_pos];
+        let mut spec_unique_print = vec![speculative_guard.pos()];
+
+        while let Some(next_spec) = speculative_guard.next() {
+            if next_spec.pos().x >= self.max_x || next_spec.pos().y >= self.max_y {
+                #[cfg(not(feature = "print"))]
+                return false;
+                #[cfg(feature = "print")]
+                return None;
+            }
+
+            if let Err(insert_pos) = spec_unique.binary_search(&next_spec) {
+                // Check if next position is an invalid tile
+                if (self
+                    .obstacles
+                    .get(&next_spec.pos().y)
+                    .map(|blocked_x| blocked_x.binary_search(&next_spec.pos().x).is_ok())
+                    == Some(true))
+                    || next_spec.pos() == block_pos
+                {
+                    // Rotate 90 and rerun
+                    speculative_guard.rotate();
+                } else {
+                    // Duplicate with primary looping
+                    if let Ok(start_idx) =
+                        unique.binary_search_by_key(&next_spec.pos(), |x| x.pos())
+                    {
+                        for idx in (0..start_idx).rev() {
+                            if unique[idx].pos() == next_spec.pos() {
+                                if unique[idx] == next_spec {
+                                    #[cfg(not(feature = "print"))]
+                                    return true;
+                                    #[cfg(feature = "print")]
+                                    return Some(spec_unique_print);
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+
+                        for idx in start_idx..unique.len() {
+                            if unique[idx].pos() == next_spec.pos() {
+                                if unique[idx] == next_spec {
+                                    #[cfg(not(feature = "print"))]
+                                    return true;
+                                    #[cfg(feature = "print")]
+                                    return Some(spec_unique_print);
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    // New tile is unique, insert and set as the new val
+                    spec_unique.insert(insert_pos, next_spec);
+                    speculative_guard = next_spec;
+
+                    #[cfg(feature = "print")]
+                    spec_unique_print.push(next_spec.pos());
+                }
+            } else {
+                // Repeat in the speculative loop
+                #[cfg(not(feature = "print"))]
+                return true;
+                #[cfg(feature = "print")]
+                return Some(spec_unique_print);
+            }
+        }
+
+        #[cfg(not(feature = "print"))]
+        return false;
+        #[cfg(feature = "print")]
+        return None;
+    }
+
+    pub fn num_possible_blockages(&self) -> usize {
+        let mut guard = self.guard;
+        let mut unique = vec![guard];
+        let mut num_blocks = 0;
+
+        #[cfg(feature = "print")]
+        let mut print_unique = vec![guard.pos()];
 
         // While still in bounds
         while guard.pos().x < self.max_x && guard.pos().y < self.max_y {
             if let Some(next_guard) = guard.next() {
                 // Checking for dupes here speeds along running repeated paths
-                if let Err(insert_pos) = unique.binary_search(&next_guard) {
+                if let Err(insert_pos) = unique.binary_search_by_key(&next_guard.pos(), |x| x.pos())
+                {
                     // Check if next position is an invalid tile
                     if self
                         .obstacles
@@ -225,93 +342,39 @@ impl GuardMap {
                         // Rotate 90 and rerun
                         guard.rotate();
                     } else {
-                        let mut speculative_guard = guard;
-                        speculative_guard.rotate();
-                        let mut overlap = false;
-
-                        let mut private_loop = vec![speculative_guard];
-
-                        if unique.binary_search(&speculative_guard).is_ok() {
-                            overlap = true
-                        } else {
-                            while speculative_guard.pos().x < self.max_x
-                                && speculative_guard.pos().y < self.max_y
-                            {
-                                if let Some(next_spec_guard) = speculative_guard.next() {
-                                    // Dupe means we've found a loop
-                                    if unique.binary_search(&next_spec_guard).is_ok() {
-                                        overlap = true;
-                                        break;
-                                    } else if let Err(loc) =
-                                        private_loop.binary_search(&next_spec_guard)
-                                    {
-                                        // Check if next position is an invalid tile
-                                        if next_spec_guard.pos() == next_guard.pos()
-                                            || self.obstacles.get(&next_spec_guard.pos().y).map(
-                                                |blocked_x| {
-                                                    blocked_x
-                                                        .binary_search(&next_spec_guard.pos().x)
-                                                        .is_ok()
-                                                },
-                                            ) == Some(true)
-                                        {
-                                            // Rotate 90 and rerun
-                                            speculative_guard.rotate();
-                                        } else {
-                                            // Continue iterating
-                                            speculative_guard = next_spec_guard;
-                                        }
-
-                                        // Bump into the private tracking loop
-                                        private_loop.insert(loc, speculative_guard);
-                                    } else {
-                                        overlap = true;
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-
-                        if overlap {
-                            #[cfg(feature = "print")]
-                            {
-                                let mut print_vec_deduped = Vec::with_capacity(print_unique.len());
-                                for pos in print_unique.iter().cloned().rev() {
-                                    if !print_vec_deduped.contains(&pos) {
-                                        print_vec_deduped.push(pos);
-                                    }
-                                }
-                                print_unique = print_vec_deduped.into_iter().rev().collect();
-                            }
-
-                            #[cfg(feature = "print")]
-                            self.print_map(
-                                &guard,
-                                print_unique.iter().cloned(),
-                                Some(next_guard.pos()),
-                                private_loop.iter().map(|x| x.pos()),
-                            );
-
-                            //println!("{:?}", guard);
-                            obstruction_pos.push(next_guard.pos());
-                            num_blocks += 1;
-                        }
+                        let loops = self.speculate_loop(&guard, &next_guard, &unique);
 
                         // New tile is unique, insert and set as the new val
                         unique.insert(insert_pos, next_guard);
-
-                        // Also add to unique locations, if applicable
-                        if let Err(second_insert_pos) = unique_pos.binary_search(&next_guard.pos())
-                        {
-                            unique_pos.insert(second_insert_pos, next_guard.pos());
-                        }
-
                         guard = next_guard;
 
                         #[cfg(feature = "print")]
                         print_unique.push(next_guard.pos());
+
+                        #[cfg(not(feature = "print"))]
+                        if loops {
+                            num_blocks += 1;
+                        }
+
+                        #[cfg(feature = "print")]
+                        if let Some(spec_unique_print) = loops {
+                            num_blocks += 1;
+
+                            let mut print_vec_deduped = Vec::with_capacity(print_unique.len());
+                            for pos in std::mem::take(&mut print_unique).into_iter().rev() {
+                                if !print_vec_deduped.contains(&pos) {
+                                    print_vec_deduped.push(pos);
+                                }
+                            }
+                            print_unique = print_vec_deduped.into_iter().rev().collect();
+
+                            self.print_map(
+                                &guard,
+                                print_unique.iter().cloned(),
+                                Some(next_guard.pos()),
+                                spec_unique_print.into_iter(),
+                            );
+                        }
                     }
                 } else {
                     // New tile is not unique, set as the new val
@@ -325,9 +388,6 @@ impl GuardMap {
             }
         }
 
-        obstruction_pos.sort_unstable();
-        obstruction_pos.dedup();
-        println!("OBS: {}", obstruction_pos.len());
         num_blocks
     }
 
@@ -373,12 +433,6 @@ impl GuardMap {
                 });
 
             {
-                let color = colors[0].to_rgba8();
-                tiles[self.guard.pos().y][self.guard.pos().x] =
-                    "^".truecolor(color[0], color[1], color[2]).to_string();
-            }
-
-            {
                 let colors_speculative: Vec<_> = colorgrad::preset::turbo()
                     .colors(speculative.len() + (speculative.len() / 5))
                     .into_iter()
@@ -396,6 +450,12 @@ impl GuardMap {
                             *tile = "$".truecolor(color[0], color[1], color[2]).to_string();
                         }
                     });
+            }
+
+            {
+                let color = colors[0].to_rgba8();
+                tiles[self.guard.pos().y][self.guard.pos().x] =
+                    "^".truecolor(color[0], color[1], color[2]).to_string();
             }
 
             tiles[guard.pos().y][guard.pos().x] = guard_char.to_string();
